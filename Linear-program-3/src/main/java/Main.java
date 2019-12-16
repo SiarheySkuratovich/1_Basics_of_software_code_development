f5e3a442a2bd07c487f37f62e9b99f9266b7ed9f
unknown <SiarheySkuratovich@yandex.ru>
2019-12-16 21:53:49 +0300

    Linear_Program_3

---
+++ Linear-program-3/src/main/java/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import static java.lang.Math.sin;
+import static java.lang.Math.cos;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Answer is: " + calc(Math.PI, Math.PI));
+
+    }
+
+    public static double calc(double x, double y) {
+        return ((sin(x) + cos(y)) /
+        cos(x) - sin(y)) * Math.tan(x * y);
+    }
+
+
+
+}
