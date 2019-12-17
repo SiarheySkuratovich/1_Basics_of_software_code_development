4514c509e447d19b0a7d51c9263da3e62e8a74a5
unknown <SiarheySkuratovich@yandex.ru>
2019-12-17 23:33:23 +0300

    CONSTANTS

--- Linear_program_5/src/main/java/Main.java
+++ Linear_program_5/src/main/java/Main.java
@@ -2,13 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        final int SEC_PER_HOUR = 3600;
+        final int SEC_PER_MIN = 60;
         System.out.println("Enter how much seconds have passed");
         Scanner scanner = new Scanner(System.in);
         int timeInSeconds = scanner.nextInt();
-        int hours = timeInSeconds / 3600;
-        int minutes = (timeInSeconds - (hours * 3600)) / 60;
-        int seconds = timeInSeconds - (hours * 3600) - (minutes * 60);
+        int hours = timeInSeconds / SEC_PER_HOUR;
+        int minutes = (timeInSeconds - (hours * SEC_PER_HOUR)) / SEC_PER_MIN;
+        int seconds = timeInSeconds - (hours * SEC_PER_HOUR) - (minutes * SEC_PER_MIN);
         System.out.println("It took: " + hours + "H " + minutes + "M " + seconds + "S");
-
     }
 }
